ee980b86f97c3d03b10967f2f8378f487ad36f33
WheelyHog <34279147+WheelyHog@users.noreply.github.com>
2017-12-11 21:35:59 +0200

    made some changes

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-$('#request').click (function() {
-    $.ajax({url: 'https://api.myjson.com/bins/1afl4z',
-            context: document.body})
+
+var requestUrl = 'http://api.apixu.com/v1/current.json?key=087ab95ab1a146eaaf294757170412';
+var city = '';
+
+$('select').change(function () {
+    $( "select option:selected" ).each(function() {
+        city = $( this ).text();
+
+        console.log(city);
+    });
+
+    $.ajax({url: requestUrl,
+           data: {'q' : city} })
         .done (function (response) {
-        for (var key in response) {
-            $('#value').get(0).innerHTML += response[key] + '<br>';
-            //alert ('Ключ: ' + key + ' Значение: ' + response[key]);
-        }
-        //console.log(response.name);
-    })
+            console.log(response);
+            console.log($('#town'));
+            $('#town').get(0).innerHTML = response.location.name;
+            $('#temp').get(0).innerHTML = response.current.temp_c;
+            $('#wind').get(0).innerHTML = response.current.wind_kph;
+            $('#img').get(0).src = response.current.condition.icon;
+
+            console.log(response.current.temp_c);
+        })
         .fail (function (response) {
             inner.get(0).innerHTML = 'An error occurred during your request: ' +  response.status + ' ' + response.statusText;
-    })
+        })
 
-    //console.log(response.readyState);
-    });
+})
